66832aef3212f8d09b3df2eb4cd602898f2a8ddd
Joakim Nohlgård <jnohlgard@spotify.com>
2020-11-18 08:28:08 +0100

    HelloWorld: WIP experiment

--- Examples/HelloWorld/HelloWorld.cpp
+++ Examples/HelloWorld/HelloWorld.cpp
@@ -1,22 +1,42 @@
-
-#include "gtest/gtest.h"
+#include <iostream>
 
 class HelloWorld {
-
 public:
+  HelloWorld() {
+    std::cout << "HelloWorld(): " << this << std::endl;
+  }
+  ~HelloWorld() {
+    std::cout << "~HelloWorld(): " << this << std::endl;
+  }
+
   int sum(int a, int b);
 };
 
 int HelloWorld::sum(int a, int b) {
   int c = a + b;
+  std::cout << "HelloWorld::sum: " << a << " + " << b << " = " << c << std::endl;
 
   return c;
 }
 
-TEST(HelloWorld, sum) {
-  HelloWorld hello;
+int summa(int a, int b) {
+  HelloWorld h;
+  return h.sum(a, b);
+}
 
-  int result = hello.sum(2, 3);
+extern "C" {
+int csumma(int a, int b) {
+  return summa(a, b);
+}
+}
 
-  EXPECT_EQ(result, 5);
+int main(int argc, char **argv) {
+  int expected = 123;
+  int a = 130;
+  int b = -7;
+  int actual = summa(a, b);
+  if (actual != expected) {
+    return 1;
+  }
+  return 0;
 }
